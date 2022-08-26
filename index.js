02585d7ba8df398837ee5aad67d7c92df2dcaf58
raposamillar <lisa.raposamillar@gmail.com>
2022-08-26 19:30:30 -0400

    add inquirer package variable and array of questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter the title of your project!');
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input', 
+    name: 'description',
+    message: 'Enter a project description. (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter a description for your project!');
+        return false;
+      }
+    }
+  },
+  { 
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions. (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter the installation instructions for your project!');
+        return false;
+      }  
+    }
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information. (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter the usage information for your project!');
+        return false;
+      }  
+    }
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide guidelines on how other developers can contribute. (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please provide contribution guidelines for other developers!');
+        return false;
+      }  
+    }
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions. (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter test instructions for your project!');
+        return false;
+      }  
+    }
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +89,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+
