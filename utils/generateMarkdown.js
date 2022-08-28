a831fe02d14700558eadea9900268238d7545505
raposamillar <lisa.raposamillar@gmail.com>
2022-08-28 12:48:22 -0400

    added a function to render the license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+  if (!license) {
+    renderLicenseBadge.license = [];
+  } else {
+    const badges = {
+      MIT:'[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+      ISC:'[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+      GPLv3:'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    }
+    return badges[license]
+  }
+} 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -17,7 +28,7 @@ function renderLicenseSection(license) {}
 // `;
 // }
 
-const generateMarkdown = data => {
+const generateMarkdown = (data) => {
   return `
   # ${data.title}
 
@@ -54,4 +65,5 @@ const generateMarkdown = data => {
   `
 };
 
-module.exports = generateMarkdown;
+
+module.exports = generateMarkdown.data;
