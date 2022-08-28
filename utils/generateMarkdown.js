a4342edc81a6e9a1ae0cff019aa3e9ff746ccfce
raposamillar <lisa.raposamillar@gmail.com>
2022-08-28 13:37:21 -0400

    add function to render the license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,7 +31,15 @@ const renderLicenseLink = (license) => {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {
+  if (!license) {
+    renderLicenseSection = [];
+  } else {
+    return `Licensed under the ${this.renderLicenseLink(license)} license`
+  }  
+}
+
 
 // TODO: Create a function to generate markdown for README
 // function generateMarkdown(data) {
